f00c4aa0602c610918f384b562347411299a8b24
Hector Rangel <hector.rangeljr@yahoo.com>
2021-06-01 19:51:49 -0500

    added db.query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -8,10 +9,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        // your mysql username
+        user: 'root',
+        // your mysql password
+        password: 'Ahri234#',
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (not found)
